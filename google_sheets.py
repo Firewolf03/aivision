13377d6447a91dd738db549f86ca10e4da54cec0
Firewolf03 <n.aisahtan@gmail.com>
2026-06-06 15:43:59 +0800

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -1,25 +1,28 @@
+import streamlit as st
+import json
 import gspread
 from google.oauth2.service_account import Credentials
-from config import SHEET_NAME
 
 class GoogleSheetService:
 
     def __init__(self):
+
         scope = [
             "https://spreadsheets.google.com/feeds",
             "https://www.googleapis.com/auth/drive"
         ]
 
-        creds = Credentials.from_service_account_file(
-            "credentials.json",
+        # read from Streamlit Secrets
+        creds_dict = json.loads(st.secrets["google_credentials"])
+
+        creds = Credentials.from_service_account_info(
+            creds_dict,
             scopes=scope
         )
 
         client = gspread.authorize(creds)
-        self.sheet = client.open(SHEET_NAME).sheet1
+
+        self.sheet = client.open("YOUR_SHEET_NAME").sheet1
 
     def append_row(self, row):
         self.sheet.append_row(row)
-
-    def get_all_rows(self):
-        return self.sheet.get_all_values()
