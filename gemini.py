76609e3274b78c10bd836762660610bdb67392a4
Firewolf03 <n.aisahtan@gmail.com>
2026-06-06 16:00:22 +0800

    Create gemini.py

---
+++ gemini.py
@@ -0,0 +1,23 @@
+from google import genai
+from google.genai import types
+import json
+
+def extract_with_gemini(api_key, image, prompt):
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=[prompt, image],
+        config=types.GenerateContentConfig(
+            temperature=0.1,
+            response_mime_type="application/json"
+        )
+    )
+
+    text = response.text.strip()
+
+    if text.startswith("```"):
+        text = text.replace("```json", "").replace("```", "").strip()
+
+    return json.loads(text)
